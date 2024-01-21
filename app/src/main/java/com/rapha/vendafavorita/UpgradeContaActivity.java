fd38e4c5edd05241ea9cd17cf94921036d0e65c4
Raphael Almeida Ribeiro <raphael.rb96@gmail.com>
2024-01-20 21:44:33 -0400

    Implementação do Container Upgrade Perfil na Screen: home

---
+++ app/src/main/java/com/rapha/vendafavorita/UpgradeContaActivity.java
@@ -0,0 +1,14 @@
+package com.rapha.vendafavorita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpgradeContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrade_conta);
+    }
+}
